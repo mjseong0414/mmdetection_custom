d35499cd854741a4939a4fa415a4f5a6200d5a78
mjseong0414 <mjseong@spa.hanyang.ac.kr>
2020-10-19 19:12:10 +0900

    update configs for data aug(expansion)

--- mmdetection/configs/_base_/datasets/coco_detection_colab.py
+++ mmdetection/configs/_base_/datasets/coco_detection_colab.py
@@ -5,6 +5,15 @@ img_norm_cfg = dict(
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.3),
     dict(type='Resize', img_scale=(1333, 800), keep_ratio=True),
     dict(type='RandomFlip', flip_ratio=0.5),
     dict(type='Normalize', **img_norm_cfg),
@@ -32,8 +41,8 @@ data = dict(
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
-        ann_file = [data_root + 'annotations/filtered_train.json'],
-        img_prefix=[data_root + 'coco_carTruck'],
+        ann_file = [data_root + 'coco_subset_images/annotations/filtered_train.json'],
+        img_prefix=[data_root + 'coco_subset_images/coco_carTruck'],
         # ann_file='data/open_images_downloader/image/annotations/carTruck_train-annotations-bbox.json',
         # img_prefix=data_root + 'train2017/',
         # img_prefix='data/open_images_downloader/image/train/',
@@ -42,12 +51,12 @@ data = dict(
         type=dataset_type,
         # ann_file=[data_root + 'annotations/instances_val2017.json', data_root + 'annotations/car_truck_openImage_val.json']
         # separate_eval = False, # 연결한 데이터들 전체를 eval
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'coco_subset_images/annotations/instances_val2017.json',
+        img_prefix=data_root + 'coco_subset_images/val2017/',
         pipeline=test_pipeline),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'annotations/instances_val2017.json',
-        img_prefix=data_root + 'val2017/',
+        ann_file=data_root + 'coco_subset_images/annotations/instances_val2017.json',
+        img_prefix=data_root + 'coco_subset_images/val2017/',
         pipeline=test_pipeline))
 evaluation = dict(interval=1, metric='bbox')
